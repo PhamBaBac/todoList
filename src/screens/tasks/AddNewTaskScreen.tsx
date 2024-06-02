14d994f55012f06665bf22e064e87a9625fa4109
Pham Ba Bac <bacphamba082@gmail.com>
2024-06-02 11:49:50 +0700

    add dropdownPicker

--- src/screens/tasks/AddNewTaskScreen.tsx
+++ src/screens/tasks/AddNewTaskScreen.tsx
@@ -1,37 +1,85 @@
-import React, { useState } from 'react'
-import Container from '../../components/Container'
+import React, {useEffect, useState} from 'react';
+import Container from '../../components/Container';
 import InputComponent from '../../components/InputComponent';
 import SectionComponent from '../../components/SectionComponent';
-import { taskModel } from '../../models/taskModel';
-import { Button, View } from 'react-native';
+import {taskModel} from '../../models/taskModel';
+import {Button, View} from 'react-native';
 import DateTimePickerComponent from '../../components/DateTimePickerComponent';
 import RowComponent from '../../components/RowComponent';
 import SpaceComponent from '../../components/SpaceComponent';
+import storage from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore';
+import {SelectModel} from '../../models/selectModel';
+import DropdownPicker from '../../components/DropdownPicker';
 
-const AddNewTaskScreen = () => {
-    const initValue: taskModel = {
-      title: '',
-      desctiption: '',
-      dueDate: new Date(),
-      start: new Date(),
-      end: new Date(),
-      uids: [],
-      fileUrls: [],
+const AddNewTaskScreen = ({navigation} : any) => {
+  const initValue: taskModel = {
+    title: '',
+    desctiption: '',
+    dueDate: new Date(),
+    start: new Date(),
+    end: new Date(),
+    uids: [],
+    fileUrls: [],
+  };
+  const [taskDetail, setTaskDetail] = useState<taskModel>(initValue);
+  const [usersSelect, setUsersSelect] = useState<SelectModel[]>([]);
+  const [attachmentsUrl, setAttachmentsUrl] = useState<string[]>([]);
+
+  useEffect(() => {
+    handleGetAllUsers();
+  }, []);
+
+  const handleChangeValue = (id: string, value: string | string[] | Date) => {
+    const item: any = {...taskDetail};
+
+    item[`${id}`] = value;
+
+    setTaskDetail(item);
+  };
+  const handleAddNewTask = async () => {
+    const data = {
+      ...taskDetail,
+      fileUrls: attachmentsUrl,
     };
-    const [taskDetail, setTaskDetail] = useState<taskModel>(initValue);
 
-    const handleChangeValue = (id: string, value: string | Date) => {
-      const item: any = {...taskDetail};
+    await firestore()
+      .collection('tasks')
+      .add(data)
+      .then(() => {
+        console.log('New task added!!');
+        navigation.goBack();
+      })
+      .catch(error => console.log(error));
+  };
 
-      item[`${id}`] = value;
+  const handleGetAllUsers = async () => {
+    await firestore()
+      .collection('users')
+      .get()
+      .then(snap => {
+        if (snap.empty) {
+          console.log(`users data not found`);
+        } else {
+          const items: SelectModel[] = [];
 
-      setTaskDetail(item);
-    };
-    const handleAddNewTask = async () => {
-      console.log(taskDetail);
-    };
+          snap.forEach(item => {
+            items.push({
+              label: item.data().name,
+              value: item.id,
+            });
+          });
+          setUsersSelect(items);
+        }
+      })
+      .catch((error: any) => {
+        console.log(`Can not get users, ${error.message}`);
+      });
+  };
+  
+
   return (
-    <Container back title="Add new task" >
+    <Container back title="Add new task">
       <SectionComponent>
         <InputComponent
           value={taskDetail.title}
@@ -77,11 +125,18 @@ const AddNewTaskScreen = () => {
           />
         </View>
       </RowComponent>
+      <DropdownPicker
+        selected={taskDetail.uids}
+        items={usersSelect}
+        onSelect={val => handleChangeValue('uids', val)}
+        title="Members"
+        multible
+      />
       <SectionComponent>
         <Button title="Save" onPress={handleAddNewTask} />
       </SectionComponent>
     </Container>
   );
-}
+};
 
-export default AddNewTaskScreen
+export default AddNewTaskScreen;
